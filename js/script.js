ed266c681c3882a761c75f16433f98cc720c31f1
Marta Majerowska <majeranek85@gmail.com>
2019-12-12 13:59:14 +0100

    Add script to create HTML code of the article link

--- js/script.js
+++ js/script.js
@@ -56,7 +56,8 @@
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
       console.log('Title: ' + articleTitle);
       /* get the title from the title element */
-
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
       /* create HTML of the link */
 
       /* insert link into titleList */
