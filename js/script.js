3bbb1dba33cddd8dec51434b959fe171868a0ab1
Marta Majerowska <majeranek85@gmail.com>
2019-12-12 07:03:00 +0100

    Add script to remove class active from links

--- js/script.js
+++ js/script.js
@@ -10,7 +10,11 @@ const titleClickHandler = function(event){
   console.log(event);
 
   /* remove class 'active' from all article links */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
