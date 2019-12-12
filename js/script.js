63264dcd28d488d7caed8a3a8887bedd2572985d
Marta Majerowska <majeranek85@gmail.com>
2019-12-12 10:55:34 +0100

    Add class active to the selected article

--- js/script.js
+++ js/script.js
@@ -25,13 +25,14 @@ const titleClickHandler = function(event){
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute form the clicked link */
+  /* [DONE] get 'href' attribute form the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active')
 }
 
 const links = document.querySelectorAll('.titles a');
